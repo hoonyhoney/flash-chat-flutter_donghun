8ba2965731db2ae90c25a57329e6cd91d7fcb302
donghun <hoonyhoeny@gmail.com>
2021-10-18 22:58:48 +0900

    코드 리펙토링 inputDecoration, rounded_button 클래스 만들고 simply 작업

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,7 +1,9 @@
+import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
 
@@ -60,67 +62,53 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Row(
+
               children: <Widget>[
                 Hero(
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60,
+                    height: 60.0,
 
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.black,
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children :[
+
+                    AnimatedTextKit(
+
+                    animatedTexts: [
+
+                      WavyAnimatedText( '꿀밤&의주채팅',textStyle: TextStyle(fontSize:30, color: Colors.black),speed: Duration(milliseconds: 300)),
+                    ],
+                    isRepeatingAnimation: true,
+                    onTap: () {
+                    },
                   ),
+                  ],
+
                 ),
               ],
             ),
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(title: 'Log in', color: Colors.lightBlueAccent,
+            onPressed: (){
+              Navigator.pushNamed(context, LoginScreen.id);
+            },),
+            RoundedButton(title: 'Register', color: Colors.blueAccent,
+            onPressed: (){
+              Navigator.pushNamed(context, RegistrationScreen.id);
+            },),
+
           ],
         ),
       ),
     );
   }
 }
+
+
