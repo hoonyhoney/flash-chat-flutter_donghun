a18a6c7f666288b11115434b25190d9fb403e715
donghun <hoonyhoeny@gmail.com>
2021-10-21 22:42:50 +0900

    firebase 설정

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 
@@ -10,6 +11,14 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  void getCurrentUser() async{
+    final user = await _auth.currentUser;
+    if (user !=null){
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
 
@@ -11,6 +14,11 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,8 +40,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
               onChanged: (value) {
-                //Do something with the user input.
+                email =value;
               },
               decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email')
             ),
@@ -41,16 +51,29 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
-              onChanged: (value) {
-                //Do something with the user input.
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                password =value;
               },
                 decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password')
             ),
             SizedBox(
               height: 24.0,
             ),
-            RoundedButton(title: 'Register', color: Colors.blueAccent, onPressed: (){
-
+            RoundedButton(title: 'Register', color: Colors.blueAccent, onPressed: () async{
+                print(email);
+                print(password);
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email, password: password);
+                  if(newUser !=null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                }
+                catch(e) {
+                  print(e);
+                }
             },)
           ],
         ),
